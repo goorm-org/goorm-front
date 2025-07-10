2b9d9927646a97d8f89f5c032e2c8010a27586cc
eungwang <scm323@naver.com>
2025-07-11 03:05:43 +0900

    bug-fix: build-error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { SWRProvider } from "./_providers/swr-provider";
 import Navigation from "@/app/_components/navigation";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "모다모영",
@@ -22,12 +23,14 @@ export default async function RootLayout({
         />
       </head>
       <body className="relative bg-primary-50">
-        <SWRProvider>
-          <main className="bg-background-2 max-w-[393px] mx-auto h-dvh">
-            {children}
-            <Navigation />
-          </main>
-        </SWRProvider>
+        <Suspense fallback={null}>
+          <SWRProvider>
+            <main className="bg-background-2 max-w-[393px] mx-auto h-dvh">
+              {children}
+              <Navigation />
+            </main>
+          </SWRProvider>
+        </Suspense>
       </body>
     </html>
   );
