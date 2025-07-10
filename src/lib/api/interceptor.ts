b9d7b2d415a318d84fe4a1ea7de85f194d18807d
eungwang <scm323@naver.com>
2025-07-10 22:58:51 +0900

    fix: axios 인스턴스의 baseURL을 ngrok URL로 변경

--- src/lib/api/interceptor.ts
+++ src/lib/api/interceptor.ts
@@ -5,7 +5,7 @@ import axios, {
 } from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "https://1dd48a047e21.ngrok-free.app",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
