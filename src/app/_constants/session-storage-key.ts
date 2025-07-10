79a4aa11fd59e98d625fda33eb76787ced29aaf7
eungwang <scm323@naver.com>
2025-07-10 21:27:28 +0900

    chore: 사용하지 않는 파일 삭제

--- src/app/_constants/session-storage-key.ts
+++ src/app/_constants/session-storage-key.ts
@@ -1,3 +1,1 @@
 export const ONBOARDING_DATA_KEY = "onboarding_data";
-
-export const PLACE_FILTER_DATA_KEY = "place_filter_data";
--- src/app/_utils/filter.ts
+++ src/app/_utils/filter.ts
@@ -1,7 +1,4 @@
-import {
-  ONBOARDING_DATA_KEY,
-  PLACE_FILTER_DATA_KEY,
-} from "../_constants/session-storage-key";
+import { ONBOARDING_DATA_KEY } from "../_constants/session-storage-key";
 import { OnboardingSchema } from "../onboarding/_schemas/onboarding_schema";
 
 /** onboarding data */
@@ -21,27 +18,6 @@ export const updateOnboardingDataToSessionStorage = (
   if (!prevFilterData) return;
   sessionStorage.setItem(
     ONBOARDING_DATA_KEY,
-    JSON.stringify({ ...prevFilterData, ...data })
-  );
-};
-
-/** place filter data */
-export const getPlaceFilterDataFromSessionStorage = () => {
-  const filterData = sessionStorage.getItem(PLACE_FILTER_DATA_KEY);
-  return filterData ? (JSON.parse(filterData) as string[]) : null;
-};
-
-export const setPlaceFilterDataToSessionStorage = (data: string[]) => {
-  sessionStorage.setItem(PLACE_FILTER_DATA_KEY, JSON.stringify(data));
-};
-
-export const updatePlaceFilterDataToSessionStorage = (
-  data: Partial<string[]>
-) => {
-  const prevFilterData = getPlaceFilterDataFromSessionStorage();
-  if (!prevFilterData) return;
-  sessionStorage.setItem(
-    PLACE_FILTER_DATA_KEY,
     JSON.stringify({ ...prevFilterData, ...data })
   );
 };
