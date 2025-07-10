022b76c5eb9f8ede0d7384929f782fe9c7a2ffb7
eungwang <scm323@naver.com>
2025-07-10 11:37:22 +0900

    feat: 온보딩 step01 페이지 및 관련 컴포넌트 추가

---
+++ src/app/onboarding/_hooks/useStep.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+
+const ONBOARDING_LAST_STEP = 3;
+
+const useStep = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const step = searchParams.get("step") ?? "1";
+
+  const nextStep = () => {
+    if (step === ONBOARDING_LAST_STEP.toString()) {
+      return;
+    }
+    const params = new URLSearchParams(searchParams);
+    params.set("step", (Number(step) + 1).toString());
+    router.push(`/onboarding?${params.toString()}`);
+  };
+
+  return {
+    step,
+    nextStep,
+  };
+};
+
+export default useStep;
