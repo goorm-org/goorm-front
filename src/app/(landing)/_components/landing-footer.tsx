0e1372951d3542d5f0e896810dc1af1cd5e0fa58
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2025-07-14 15:51:07 +0900

    fix: onboarding 페이지 footer bg color 수정

--- src/app/(landing)/_components/landing-footer.tsx
+++ src/app/(landing)/_components/landing-footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function LandingFooter() {
   return (
     <Card.Root className="bg-transparent border-none">
-      <Card.Footer className="flex flex-col gap-[8px] h-30 items-center justify-center">
+      <Card.Footer className="flex h-full items-center justify-center bg-[#e8e8e8]">
         <Link href="/onboarding" className="w-full">
           <Button stretch size="lg" className="bg-primary-700">
             Go
