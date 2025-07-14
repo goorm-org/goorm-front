3e391c3bfc49ec0f1ad1611548b6f42297d0435d
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2025-07-14 14:47:47 +0900

    feat: 일정 생성 skip 버튼 제거

--- src/app/(landing)/_components/landing-footer.tsx
+++ src/app/(landing)/_components/landing-footer.tsx
@@ -1,31 +1,15 @@
-"use client";
-
 import { Button, Card } from "@vapor-ui/core";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 
 export default function LandingFooter() {
-  const router = useRouter();
-  const onClickSkip = () => {
-    router.push("/map");
-  };
   return (
     <Card.Root className="bg-transparent border-none">
-      <Card.Footer className="flex flex-col gap-[8px]">
-        <Link href="/onboarding">
+      <Card.Footer className="flex flex-col gap-[8px] h-30 items-center justify-center">
+        <Link href="/onboarding" className="w-full">
           <Button stretch size="lg" className="bg-primary-700">
             Go
           </Button>
         </Link>
-        <Button
-          stretch
-          size="lg"
-          variant="ghost"
-          className="text-black underline"
-          onClick={onClickSkip}
-        >
-          SKIP
-        </Button>
       </Card.Footer>
     </Card.Root>
   );
